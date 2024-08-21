d4c782d4db3b6035524dfe53c082092cb317c7e2
Ferrin <luke.acuff@gmail.com>
2024-08-21 00:38:06 -0400

    Ported Simple BLock Physics from 1.29.1 and added falling block behavior

---
+++ src/main/java/net/ferrinember/simpleblockphysics/SimpleBlockPhysics.java
@@ -0,0 +1,57 @@
+package net.ferrinember.simpleblockphysics;
+
+import com.mojang.logging.LogUtils;
+import net.ferrinember.simpleblockphysics.utils.BlockBreakHandler;
+import net.ferrinember.simpleblockphysics.utils.TickHandler;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SimpleBlockPhysics.MOD_ID)
+public class SimpleBlockPhysics
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "simpleblockphysics";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    public SimpleBlockPhysics()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        MinecraftForge.EVENT_BUS.register(new BlockBreakHandler());
+        MinecraftForge.EVENT_BUS.register(new TickHandler());
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+
+        }
+    }
+}
